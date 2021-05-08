98267c46ae332c397713a04fa0965bccb8594dc7
a2p1k02 <35633190+a2p1k02@users.noreply.github.com>
2021-05-08 20:04:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("D:/cygwin64/home/a2p1k02/Projects/c_cpp/vyu/src/vyu/icon/eye.png"));
+    a.setWindowIcon(QIcon("FIX THIS"));
     MainWindow w;
     w.show();
     FreeConsole();
