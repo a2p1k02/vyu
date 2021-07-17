b76dce9c8b79061cd6c4455bcb2d7d5fe5e2b7d5
a2p1k02 <35633190+a2p1k02@users.noreply.github.com>
2021-07-17 09:14:19 +0300

    Update mainwindow.cpp

--- src/vyu/mainwindow.cpp
+++ src/vyu/mainwindow.cpp
@@ -9,81 +9,95 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) 
+{
     ui->setupUi(this);
     ui->imageViewer->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
     ui->imageViewer->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow() 
+{
     delete ui;
 }
 
-bool MainWindow::getFile(QString file) {
+bool MainWindow::getFile(QString file) 
+{
     if (file != "") {
         showImage(file);
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 
-void MainWindow::zoomIn() {
+void MainWindow::zoomIn() 
+{
     zoomImage(1.15);
     std::cout << "Zoom In" << std::endl; //Debug function
 }
 
-void MainWindow::zoomOut() {
+void MainWindow::zoomOut() 
+{
     zoomImage(0.87);
     std::cout << "Zoom Out" << std::endl; //Debug function
 }
 
-QString MainWindow::getFilePath() {
+QString MainWindow::getFilePath() 
+{
     return QFileDialog::getOpenFileName(this, QFileDialog::tr("Open file"), "C:/",
                                         QFileDialog::tr("Image Files (*.png *.jpg *.bmp *.jpeg)"));
 }
 
-void MainWindow::showImage(QString filePath) {
+void MainWindow::showImage(QString filePath) 
+{
     img = new QPixmap(filePath);
     ui->imageViewer->setPixmap(img->scaled(ui->imageViewer->width(), ui->imageViewer->height(),
                                            Qt::KeepAspectRatio, Qt::TransformationMode::SmoothTransformation));
 }
 
-void MainWindow::zoomImage(double factor) {
+void MainWindow::zoomImage(double factor) 
+{
     scaleFactor *= factor;
     ui->imageViewer->setPixmap(img->scaled(ui->imageViewer->width() * scaleFactor, ui->imageViewer->height() * scaleFactor,
                                            Qt::KeepAspectRatio, Qt::TransformationMode::SmoothTransformation));
 }
 
-void MainWindow::zoomReset() {
+void MainWindow::zoomReset() 
+{
     ui->imageViewer->setPixmap(img->scaled(ui->imageViewer->width(), ui->imageViewer->height(),
                                            Qt::KeepAspectRatio, Qt::TransformationMode::SmoothTransformation));
     scaleFactor = 1.0;
 }
 
-void MainWindow::on_actionOpen_triggered() {
+void MainWindow::on_actionOpen_triggered() 
+{
     showImage(getFilePath());
 }
 
-void MainWindow::on_actionExit_triggered() {
+void MainWindow::on_actionExit_triggered() 
+{
     qApp->quit();
 }
 
-void MainWindow::on_actionZoom_In_triggered() {
+void MainWindow::on_actionZoom_In_triggered() 
+{
     zoomIn();
 }
 
-void MainWindow::on_actionZoom_Out_triggered() {
+void MainWindow::on_actionZoom_Out_triggered() 
+{
     zoomOut();
 }
 
-void MainWindow::on_actionZoom_Reset_triggered() {
+void MainWindow::on_actionZoom_Reset_triggered() 
+{
     zoomReset();
 }
 
-void MainWindow::on_actionAbout_triggered() {
+void MainWindow::on_actionAbout_triggered() 
+{
     QMessageBox::about(this, tr("About vyu"),
                        tr("<p><center>Vyu is Open Source lightweight image viewer was made with GPL V3 License by a2p1k02"
                           "</center></p><br><a link href='https://github.com/a2p1k02/vyu'><center>Source Code</center></a>"));
 }
-
